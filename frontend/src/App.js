109ae407c056b48eae0288278ff1e04b2c4a06ea
samriddhitiwary <151003533+samriddhitiwary@users.noreply.github.com>
2025-02-16 18:39:51 +0530

    Removed Extra Compo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,23 +20,6 @@ import BookAppointment from './components/Appoinments/BookAppointment';
 import ViewAppointments from './components/Appoinments/ViewAppointments';
 import ViewHealthRecord from './components/Appoinments/ViewHealthRecord';
 import Diagnostic from './components/Appoinments/Diagnostic/Diagnostic';
-import AbdominalTest from './components/Appoinments/Diagnostic/AbdominalTest';
-import EchoTest from './components/Appoinments/Diagnostic/EchoTest';
-import Allergy_tests from './components/Appoinments/Diagnostic/Allergy_tests';
-import Angiogram from './components/Appoinments/Diagnostic/Angiogram';
-import Ankle_brachial_index from './components/Appoinments/Diagnostic/Ankle_brachial_index';
-import Antinuclear_antibody_test from './components/Appoinments/Diagnostic/Antinuclear_antibody_test';
-import Basic_metabolic_panel from './components/Appoinments/Diagnostic/Basic_metabolic_panel';
-import Biophysical_profile from './components/Appoinments/Diagnostic/Biophysical_profile';
-import Biopsy from './components/Appoinments/Diagnostic/Biopsy';
-import Blood_pressure_test from './components/Appoinments/Diagnostic/Blood_pressure_test';
-import Blood_urea_nitrogen from './components/Appoinments/Diagnostic/Blood_urea_nitrogen';
-import Breast_biopsy from './components/Appoinments/Diagnostic/Breast_biopsy';
-import Cancer_screening from './components/Appoinments/Diagnostic/Cancer_screening';
-import Cerebral_angiogram from './components/Appoinments/Diagnostic/Cerebral_angiogram';
-import Discogram from './components/Appoinments/Diagnostic/Discogram';
-import Holter_monitor from './components/Appoinments/Diagnostic/Holter_monitor';
-import Afp_test from './components/Appoinments/Diagnostic/Afp_test';
 import Chatbot from './components/Chatbot/Chatbot';
 import AppointmentsWidget from './components/AppointmentsWidget/AppointmentsWidget';
 import Nutrients_Value from './components/Appoinments/Nutrients_Value';
@@ -76,59 +59,7 @@ function App() {
         
 
        
-        <Route path="/abdominal-ultrasound" element={<DiagnosticLayout />}>
-          <Route path="/abdominal-ultrasound" element={<AbdominalTest />} />
-        </Route>
-        <Route path="/2d-echo-test" element={<DiagnosticLayout />}>
-          <Route path="/2d-echo-test" element={<EchoTest />} />
-        </Route>
-        <Route path="/afp-test" element={<DiagnosticLayout />}>
-          <Route path="/afp-test" element={<Afp_test />} />
-        </Route>
-        <Route path="/angiogram" element={<DiagnosticLayout />}>
-          <Route path="/angiogram" element={<Angiogram />} />
-        </Route>
-        <Route path="/ankle-brachial-index" element={<DiagnosticLayout />}>
-          <Route path="/ankle-brachial-index" element={<Ankle_brachial_index />} />
-        </Route>
-        <Route path="/antinuclear-antibody-test" element={<DiagnosticLayout />}>
-          <Route path="/antinuclear-antibody-test" element={<Antinuclear_antibody_test />} />
-        </Route>
-        <Route path="/basic-metabolic-panel" element={<DiagnosticLayout />}>
-          <Route path="/basic-metabolic-panel" element={<Basic_metabolic_panel />} />
-        </Route>
-        <Route path="/biophysical-profile" element={<DiagnosticLayout />}>
-          <Route path="/biophysical-profile" element={<Biophysical_profile />} />
-        </Route>
-        <Route path="/biopsy" element={<DiagnosticLayout />}>
-          <Route path="/biopsy" element={<Biopsy />} />
-        </Route>
-        <Route path="/blood-pressure-test" element={<DiagnosticLayout />}>
-          <Route path="/blood-pressure-test" element={<Blood_pressure_test />} />
-        </Route>
-        <Route path="/blood-urea-nitrogen" element={<DiagnosticLayout />}>
-          <Route path="/blood-urea-nitrogen" element={<Blood_urea_nitrogen />} />
-        </Route>
-        <Route path="/breast-biopsy" element={<DiagnosticLayout />}>
-          <Route path="/breast-biopsy" element={<Breast_biopsy />} />
-        </Route>
-        <Route path="/cancer-screening" element={<DiagnosticLayout />}>
-          <Route path="/cancer-screening" element={<Cancer_screening />} />
-        </Route>
-        <Route path="/cerebral-angiogram" element={<DiagnosticLayout />}>
-          <Route path="/cerebral-angiogram" element={<Cerebral_angiogram />} />
-        </Route>
-        <Route path="/discogram" element={<DiagnosticLayout />}>
-          <Route path="/discogram" element={<Discogram />} />
-        </Route>
-        <Route path="/holter-monitor" element={<DiagnosticLayout />}>
-          <Route path="/holter-monitor" element={<Holter_monitor />} />
-        </Route>
-        
-        <Route path="/allergy-tests" element={<DiagnosticLayout />}>
-          <Route path="/allergy-tests" element={<Allergy_tests />} />
-        </Route>
-
+       
 
         {/* Only Header is rendered for /login route */}
         <Route path="/login" element={<HeaderOnlyLayout />}>
